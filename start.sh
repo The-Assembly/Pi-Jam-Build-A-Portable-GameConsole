88f60faf35b7bcbef3ad8e8ab4753b90d1cc79eb
Asif Rasheed <b00073877@aus.edu>
2020-02-26 14:34:16 +0400

    Adding bash script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#Author: Asif Rasheed
+
+COMMAND='python3 RAT.py'
+LOGFILE=log.txt
+
+now=`date`
+echo "$now: Started \n" >> log.txt
+while true ; do
+  $COMMAND
+  now=`date`
+  echo "$now: Exited with status $? \n" >> log.txt 
+  echo "Restarting" 
+done
